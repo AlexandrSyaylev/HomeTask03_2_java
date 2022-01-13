e2e7687738920a319dfdcd364945e0e6ab190e53
Alexander Syayalev <alexxs@inbox.ru>
2022-01-13 13:07:19 +0300

    Reformat Code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public float calculate (float height, int weight) {
-        float result = weight/(height*height/10000);
+    public float calculate(float height, int weight) {
+        float result = weight / (height * height / 10000);
         return result;
     }
 }
